773f624349f9305de39eaf9e1d529fb8a74b2033
MD Zunaeed Imran <124905281+Zunaeed-Imran@users.noreply.github.com>
2023-07-04 17:03:29 +0600

    delete.php

--- delete.php
+++ delete.php
@@ -1,25 +1,15 @@
 <?php
 include 'connect.php';
+if(isset($_GET['deleteid'])){
+  $id = $_GET['deleteid'];
+
+  $sql = "delete from `address2` where id = $id";
+  $result = mysqli_query($conn, $sql);
+  if($result){
+    header('location:display.php');
+  }else{
+    die(mysqli_error($conn));
+  }
+}
+
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-</head>
-
-<body>
-
-
-
-
-
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
-</body>
-
-</html>
